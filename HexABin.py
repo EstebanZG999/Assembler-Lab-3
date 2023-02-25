5dccd94e4edbdc0decdffd015109b82d8681e47e
EstebanZG999 <98130504+EstebanZG999@users.noreply.github.com>
2023-02-25 10:47:26 -0600

    Delete HexABin.py

--- HexABin.py
+++
@@ -1,9 +0,0 @@
-#Assembler
-#Edwin Ortega Kou 22305
-#Esteban Zambrano 22119
-
-hex = input("Ingresar un numero en hexadecimal de 3 digitos: ")
-dec = int(hex, 16)
-bin = bin(dec)[2:].zfill(12)
-
-print("La representación de binario de este numero hexadecimal", hex, "es:", bin)
