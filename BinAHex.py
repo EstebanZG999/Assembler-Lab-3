ed70b5b41bb5787ea793bcff3c830f38d70e887f
EstebanZG999 <98130504+EstebanZG999@users.noreply.github.com>
2023-02-25 10:41:15 -0600

    Binario a hexadecimal

---
+++ BinAHex.py
@@ -0,0 +1,13 @@
+binn = input("Ingresar un numero en binario de 12 digitos: ")
+
+
+while len(binn) % 4 != 0:
+    binn = '0' + binn
+
+dec = int(binn, 2)
+
+hex = hex(dec)
+
+hex = hex[2:]
+
+print("La conversion de binario de", binn, "a hexadecimal es:", hex)
